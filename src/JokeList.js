00df1701992a597a2e454b0a8d7c89f4ca9dd808
Swarleys <elessar29@gmail.com>
2019-05-25 23:19:48 +0200

    Joke component, dynamic colors and emojis

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,42 +1,66 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./JokeList.css";
+import Joke from "./Joke";
 
 export default class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10
   };
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-       jokes: []
-    }
+      jokes: []
+    };
+    this.handleVote = this.handleVote.bind(this);
   }
 
   async componentDidMount() {
     let jokes = [];
-    while(jokes.length < this.props.numJokesToGet){
+    while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com", {
         headers: {
           Accept: "application/json"
         }
       });
-      jokes.push(res.data.joke);
+      jokes.push({ text: res.data.joke, votes: 0, id: res.data.id });
     }
-    this.setState({ jokes: jokes })
+    this.setState({ jokes: jokes });
   }
+  handleVote(id, delta) {
+    this.setState(prevState => ({
+      jokes: prevState.jokes.map(joke =>
+        joke.id === id ? { ...joke, votes: joke.votes + delta } : joke
+      )
+    }));
+  }
+
   render() {
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
-          <h1 className="JokeList-title"> <span>Dad</span> Jokes</h1>
-          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="Smiley face"/>
+          <h1 className="JokeList-title">
+            {" "}
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="Smiley face"
+          />
           <button className="JokeList-getmore">New Jokes</button>
         </div>
 
         <div className="JokeList-jokes">
-          {this.state.jokes.map(joke => <p>{joke}</p> )}
+          {this.state.jokes.map(joke => (
+            <Joke
+              votes={joke.votes}
+              text={joke.text}
+              key={joke.id}
+              upvote={() => this.handleVote(joke.id, 1)}
+              downvote={() => this.handleVote(joke.id, -1)}
+            />
+          ))}
         </div>
       </div>
     );
